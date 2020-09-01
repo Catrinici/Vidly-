4f5d965b937f6eb5124c198532a84fc13ea4cb4d
Catrinici <catrinici.antonina@gmail.com>
2020-09-01 16:57:41 -0700

    second commit

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-import Like from './common/like';
+import MoviesTable from './moviesTable';
+import Pagination from './common/pagination';
+import { paginate } from '../utils/paginate';
+import ListGroup from './common/listGroup';
+import { getGenres } from '../services/fakeGenreService';
+import _ from 'lodash';
+
 
 class Movies extends Component {
 	state = {
-		movies: getMovies()
+		movies: getMovies(),
+		pageSize: 4,
+		currentPage: 1,
+		genres: [],
+		sortColumn: {path: 'title', order: 'asc'}
 	};
+
+	componentDidMount() {
+		const genres = [{ _id: "", name: 'All Genres' }, ...getGenres()];
+
+		this.setState({ movies: getMovies(), genres });
+	}
+
+	handlePageChange = (page) => {
+		this.setState({currentPage:page})
+	}
+
+	handleGenreSelect = genre => {
+		this.setState({selectedGenre: genre, currentPage:1})
+	}
 
 	handleLike = (movie) => {
 		const movies = [...this.state.movies];
@@ -20,48 +44,55 @@ class Movies extends Component {
 		this.setState({ movies });
 	}
 
+	handleSort = sortColumn => {
+		this.setState({sortColumn });
+	}
+
 	render() {
 		const { length: count } = this.state.movies;
+		const {
+			pageSize,
+			currentPage,
+			sortColumn,
+			selectedGenre,
+			movies: allMovies,
+		} = this.state;
 
 		if (count === 0) return <p>There are no movies in the database</p>
 
-		return (
-			<React.Fragment>
-				<p>Showing {count} movies in the database </p>
-				<table className="table">
+		const filtered =
+			selectedGenre && selectedGenre._id
+				? allMovies.filter(m => m.genre._id === selectedGenre._id)
+				: allMovies;
 
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Genre</th>
-							<th>Stock</th>
-							<th>Rate</th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.movies.map(movie => (
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<Like
-										liked={movie.liked}
-										onClick={() => this.handleLike(movie)}
-									
-									/>
-								</td>
-								<td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-							</tr>
-						))}
-					</tbody>
+		const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
 
-				</table>
-		
-			</React.Fragment>
-		);
+		const movies = paginate(sorted, currentPage, pageSize)
+
+		return <div className="row">
+			<div className="col-3">
+				<ListGroup items={this.state.genres}
+					onItemSelect={this.handleGenreSelect}
+					selectedItem={this.state.selectedGenre}
+				/>
+			</div>
+			<div className="col">
+					<p>Showing {filtered.length} movies in the database </p>
+				<MoviesTable
+					movies={movies}
+					sortColumn={sortColumn}
+					onLike={this.handleLike}
+					onDelete={this.handleDelete}
+					onSort = {this.handleSort}
+				/>
+				<Pagination
+					itemsCount={filtered.length}
+					pageSize={pageSize}
+					onPageChange={this.handlePageChange}
+					currentPage={currentPage}
+				/>
+			</div>
+			</div>
 	}
 	
 }
---
+++ src/utils/paginate.js
@@ -0,0 +1,8 @@
+import _ from 'lodash';
+
+export function paginate(items, pageNumber, pageSize) {
+	const starIndex = (pageNumber - 1) * pageSize;
+	return _(items).slice(starIndex).take(pageSize).value();
+	// _.slice(items, starIndex)
+	//_.take()
+}
